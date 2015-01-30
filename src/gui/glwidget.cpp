cdcaf8d5cd6c35fd0c85a42e01dc75fbf202ab24
CecileP <cecile.peladan@gmail.com>
2015-01-30 18:42:09 +0100

    suite du changement sur le preprocesseur

--- src/gui/glwidget.cpp
+++ src/gui/glwidget.cpp
@@ -2,11 +2,10 @@
 
 #if defined(__APPLE__) && (__MACH__)
     #include <GLUT/glut.h>
-#elif defined(__WINDOWS__)
+#elif defined(__WIN32__)
     #define GLUT_DISABLE_ATEXIT_HACK
     #include <GL/glut.h>
 #else
-    #define GLUT_DISABLE_ATEXIT_HACK
     #include <GL/glut.h>
 #endif
 
--- src/model/MCylinder.cpp
+++ src/model/MCylinder.cpp
@@ -1,10 +1,9 @@
 #include "model/MCylinder.h"
 #if defined(__APPLE__) && (__MACH__)
     #include <GLUT/glut.h>
-#elif defined(__WINDOWS__)
+#elif defined(__WIN32__)
     #define GLUT_DISABLE_ATEXIT_HACK
     #include <GL/glut.h>
 #else
-    #define GLUT_DISABLE_ATEXIT_HACK
     #include <GL/glut.h>
 #endif
