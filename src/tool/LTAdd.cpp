e51a05745eabd6ce4b8df8c1a1c283bf6ba5c652
Slvr <slvrgauthier@gmail.com>
2015-02-05 11:21:09 +0100

    Ajout de nombreux boutons

---
+++ src/tool/LTAdd.cpp
@@ -0,0 +1,6 @@
+#include "tool/LTInflate.h"
+
+void LTInflate::action(Model *model, QPoint last_position, QPoint current_position, float distance, float x_rot, float y_rot, float z_rot)
+{
+    model->getSize();last_position.isNull();current_position.isNull();if(distance==x_rot&&y_rot==z_rot){};
+}
---
+++ src/tool/LTAdd.h
@@ -0,0 +1,12 @@
+#ifndef LTINFLATE_H
+#define LTINFLATE_H
+
+#include "tool/Tool.h"
+
+class LTInflate : public Tool
+{
+public:
+    void action(Model *model, QPoint last_position, QPoint current_position, float distance, float x_rot, float y_rot, float z_rot);
+};
+
+#endif // LTINFLATE_H
